fcf4fad99b73a6dd405ce23ea0d3ddf72a01520b
ILLYA SMYK <smykillya@gmail.com>
2025-06-23 12:59:51 +0200

    3

--- js/task-1.js
+++ js/task-1.js
@@ -1,18 +1,4 @@
-// Початковий варіант
-function slugify(title) {
-    const slug = title.toLowerCase();
-    const slugmassive = slug.split (" ");
-    const slugdefis = slugmassive.join ("-");
-    return slugdefis;
-}
 
-
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
-// Готовий варіант
 
 function slugify(title) {
     return title.toLowerCase().split(" ").join("-");
